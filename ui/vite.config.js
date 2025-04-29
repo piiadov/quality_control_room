8109c211013ae5eeef1081c0ac4b156273167bc8
Vasilii Piiadov <piyadov@alumni.usp.br>
2025-04-28 22:12:29 -0300

    cssnano

--- ui/vite.config.js
+++ ui/vite.config.js
@@ -1,7 +1,18 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import cssnano from 'cssnano';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  css: {
+    postcss: {
+      plugins: [
+        cssnano({
+          preset: 'default',
+        }),
+      ],
+    },
+  },
 })
+
